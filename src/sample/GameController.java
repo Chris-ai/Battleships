fa93cb9587894797fd5cb20b7f166ae9ce30e6c0
Krzysiek <krzysiek.kondracikowski@gmail.com>
2020-01-21 18:42:36 +0100

    Drobny refaktoring kodu

--- src/sample/GameController.java
+++ src/sample/GameController.java
@@ -30,12 +30,13 @@ public class GameController implements Initializable {
     lab78,lab79,lab80, lab81, lab82, lab83, lab84, lab85, lab86, lab88,lab89,lab90, lab91,lab92, lab93,
      lab94, lab95, lab87, lab96, lab97, lab98,lab99;
 
-      private BoardModel board;
-      private BoardModel myBoard;
-      private Button[][] buttonArray = new Button[10][10];
-      private java.awt.Label[][] labelArray = new java.awt.Label[10][10];
-      private Computer c;
+   public List<Label> labels = new ArrayList<>();
 
+      private BoardModel ComputerBoard;
+      private BoardModel PlayerBoard;
+      private Button[][] buttonArray = new Button[10][10];
+      private Label[][] labelArray = new Label[10][10];
+      private Computer c;
 
       public Button but00;    public Button but01;
       public Button but02;    public Button but03;
@@ -115,136 +116,145 @@ public class GameController implements Initializable {
                   pomX=i;
                   pomY=j;
                   break;
-
                }
 
             }
          }
-         if (board.getPos(pomX,pomY)!=0){
-            buttonArray[pomX][pomY].setTextFill(Color.GREEN);
+         if (ComputerBoard.getPos(pomX,pomY)!=0){
+            buttonArray[pomX][pomY].setBackground(shooted);
 
-            checkDamage(pomX,pomY,board);
+            checkDamage(pomX,pomY, ComputerBoard);
             Position p = c.easyShot(10);
 
 
-            if (board.getPos(p.getX(),p.getY())!=0){
-               buttonArray[p.getX()][p.getY()].setBackground(shooted);
-               checkDamage(p.getX(),p.getY(),board);
+            if (ComputerBoard.getPos(p.getX(),p.getY())!=0){
+               labelArray[p.getX()][p.getY()].setBackground(shooted);
+               checkDamage(p.getX(),p.getY(), ComputerBoard);
             }else{
-               buttonArray[p.getX()][p.getY()].setBackground(missed);
+               labelArray[p.getX()][p.getY()].setBackground(missed);
             }
 
          }else{
             buttonArray[pomX][pomY].setBackground(missed);
             Position p = c.easyShot(10);
-            for(i = 0; i<10; i++){
 
-            }
-            if (board.getPos(p.getX(),p.getY())!=0){
-               buttonArray[p.getX()][p.getY()].setBackground(shooted);
-               checkDamage(p.getX(),p.getY(),board);
+            if (ComputerBoard.getPos(p.getX(),p.getY())!=0){
+               labelArray[p.getX()][p.getY()].setBackground(shooted);
+               checkDamage(p.getX(),p.getY(), ComputerBoard);
             }else{
-               buttonArray[p.getX()][p.getY()].setBackground(missed);
+               labelArray[p.getX()][p.getY()].setBackground(missed);
             }
          }
-
-
       }
-      public void drown (ShipModel s){
+
+   /**
+    * Metoda odpowiadająca za kolorowanie przycisków[statku] na kolor niebieski gdy zostanie on zniszczony.
+    * @param ship
+    */
+   public void ChangeDestroyedShipColor(ShipModel ship){
          int n=0;
+         int ship_orientation = ship.getOrientation(); // 1 - pozioma 0 - pionowa
 
-
-         if(s.getOrientation()==0){
-            for(int k =0;k<s.getLength();k++){
-
-               buttonArray[s.getX()][s.getY()+n].setBackground(destroyed);
-               buttonArray[s.getX()][s.getY()+n].setDisable(true);
+      switch (ship_orientation){
+         case 1:
+            for(int k =0;k<ship.getLength();k++){
+               buttonArray[ship.getX()+n][ship.getY()].setBackground(destroyed);
+               buttonArray[ship.getX()+n][ship.getY()].setDisable(true);
                n++;
             }
-         }else{
-            for(int k =0;k<s.getLength();k++){
-
-               buttonArray[s.getX()+n][s.getY()].setTextFill(Color.BLUE);
-               buttonArray[s.getX()+n][s.getY()].setDisable(true);
-               n++;
+            break;
+         case 0:
+            for(int k =0;k<ship.getLength();k++){
+            buttonArray[ship.getX()][ship.getY()+n].setBackground(destroyed);
+            buttonArray[ship.getX()][ship.getY()+n].setDisable(true);
+            n++;
             }
-
-
-         }
+            break;
+        }
       }
+
+
       public void checkDamage(int x, int y, BoardModel board){
          for (ShipModel s:board.getShipList()) {
-            if(s.getOrientation()==0){
-               if(s.getLength()==3){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()&&y==s.getY()+1)||(x==s.getX()&&y==s.getY()+2)||(x==s.getX()&&y==s.getY()+3)){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+            int length = s.getLength(); // długość statku
+
+            if (s.getOrientation() == 0) {
+
+               switch (length) {
+
+                  case 5:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() && y == s.getY() + 1) || (x == s.getX() && y == s.getY() + 2) || (x == s.getX() && y == s.getY() + 3) || (x == s.getX() && y == s.getY() + 4) || (x == s.getX() && y == s.getY() + 5)) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==5){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()&&y==s.getY()+1)||(x==s.getX()&&y==s.getY()+2)||(x==s.getX()&&y==s.getY()+3)||(x==s.getX()&&y==s.getY()+4)||(x==s.getX()&&y==s.getY()+5)){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 4:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() && y == s.getY() + 1) || (x == s.getX() && y == s.getY() + 2) || (x == s.getX() && y == s.getY() + 3) || (x == s.getX() && y == s.getY() + 4)) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==4){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()&&y==s.getY()+1)||(x==s.getX()&&y==s.getY()+2)||(x==s.getX()&&y==s.getY()+3)||(x==s.getX()&&y==s.getY()+4)){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 3:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() && y == s.getY() + 1) || (x == s.getX() && y == s.getY() + 2) || (x == s.getX() && y == s.getY() + 3)) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==2){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()&&y==s.getY()+1)||(x==s.getX()&&y==s.getY()+2)){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 2:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() && y == s.getY() + 1) || (x == s.getX() && y == s.getY() + 2)) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
+                     break;
                }
 
-            }else{
-               if(s.getLength()==5){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()+1&&y==s.getY())||(x==s.getX()+2&&y==s.getY())||(x==s.getX()+3&&y==s.getY())||(x==s.getX()+4&&y==s.getY())||(x==s.getX()+5&&y==s.getY())){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+            } else {
+
+               switch (length) {
+                  case 5:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() + 1 && y == s.getY()) || (x == s.getX() + 2 && y == s.getY()) || (x == s.getX() + 3 && y == s.getY()) || (x == s.getX() + 4 && y == s.getY()) || (x == s.getX() + 5 && y == s.getY())) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==4){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()+1&&y==s.getY())||(x==s.getX()+2&&y==s.getY())||(x==s.getX()+3&&y==s.getY())||(x==s.getX()+4&&y==s.getY())){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 4:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() + 1 && y == s.getY()) || (x == s.getX() + 2 && y == s.getY()) || (x == s.getX() + 3 && y == s.getY()) || (x == s.getX() + 4 && y == s.getY())) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==3){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()+1&&y==s.getY())||(x==s.getX()+2&&y==s.getY())||(x==s.getX()+3&&y==s.getY())){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 3:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() + 1 && y == s.getY()) || (x == s.getX() + 2 && y == s.getY()) || (x == s.getX() + 3 && y == s.getY())) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
-               }
-               if(s.getLength()==2){
-                  if((x==s.getX()&&y==s.getY())||(x==s.getX()+1&&y==s.getY())||(x==s.getX()+2&&y==s.getY())){
-                     s.damageShip();
-                     if(s.getDemage()==s.getLength()){
-                        drown(s);
+                     break;
+                  case 2:
+                     if ((x == s.getX() && y == s.getY()) || (x == s.getX() + 1 && y == s.getY()) || (x == s.getX() + 2 && y == s.getY())) {
+                        s.damageShip();
+                        if (s.getDemage() == s.getLength()) {
+                           ChangeDestroyedShipColor(s);
+                        }
                      }
-                  }
+                     break;
                }
             }
-
-         }}
-
+         }
+   }
 
       @Override
       public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -261,13 +271,15 @@ public class GameController implements Initializable {
          pomShipList.add(s2);
          pomShipList.add(s3);
 
-         board =new BoardModel(10, 10, new ManualArrange(),pomShipList);
+         ComputerBoard =new BoardModel(10, 10, new ManualArrange(),pomShipList);
+         PlayerBoard = new BoardModel(10,10,new ManualArrange(),pomShipList);
 
-         board.setShipBoard();
+         ComputerBoard.setShipBoard();
+         PlayerBoard.setShipBoard();
 
-         board.printBoardpom();
-
-
+         ComputerBoard.printBoardpom();
+         System.out.println("\n\n\n");
+         PlayerBoard.printBoardpom();
 
          buttonArray[0][0]= but00;        buttonArray[0][1]= but01;        buttonArray[0][2]= but02;        buttonArray[0][3]= but03;
          buttonArray[0][4]= but04;        buttonArray[0][5]= but05;        buttonArray[0][6]= but06;        buttonArray[0][7]= but07;
@@ -285,7 +297,7 @@ public class GameController implements Initializable {
          buttonArray[4][8]= but48;        buttonArray[4][9]= but49;
          buttonArray[5][0]= but50;        buttonArray[5][1]= but51;        buttonArray[5][2]= but52;        buttonArray[5][3]= but53;        buttonArray[5][4]= but54;        buttonArray[5][5]= but55;
          buttonArray[5][6]= but56;        buttonArray[5][7]= but57;        buttonArray[5][8]= but58;        buttonArray[5][9]= but59;        buttonArray[6][0]= but60;        buttonArray[6][1]= but61;
-         buttonArray[6][2]= but62;        buttonArray[6][3]= but63;        buttonArray[6][4]= but64;       buttonArray[6][5]= but65;        buttonArray[6][6]= but66;        buttonArray[6][7]= but67;
+         buttonArray[6][2]= but62;        buttonArray[6][3]= but63;        buttonArray[6][4]= but64;        buttonArray[6][5]= but65;        buttonArray[6][6]= but66;        buttonArray[6][7]= but67;
          buttonArray[6][8]= but68;        buttonArray[6][9]= but69;        buttonArray[7][0]= but70;        buttonArray[7][1]= but71;
          buttonArray[7][2]= but72;        buttonArray[7][3]= but73;        buttonArray[7][4]= but74;        buttonArray[7][5]= but75;
          buttonArray[7][6]= but76;        buttonArray[7][7]= but77;        buttonArray[7][8]= but78;        buttonArray[7][9]= but79;        buttonArray[8][0]= but80;
@@ -294,6 +306,17 @@ public class GameController implements Initializable {
          buttonArray[8][9]= but89;        buttonArray[9][0]= but90;        buttonArray[9][1]= but91;        buttonArray[9][2]= but92;
          buttonArray[9][3]= but93;        buttonArray[9][4]= but94;        buttonArray[9][5]= but95;        buttonArray[9][6]= but96;
          buttonArray[9][7]= but97;        buttonArray[9][8]= but98;        buttonArray[9][9]= but99;
+
+         labelArray[0][0] = lab00; labelArray[0][1] = lab01; labelArray[0][2] = lab02; labelArray[0][3] = lab03; labelArray[0][4] = lab04; labelArray[0][5] = lab05; labelArray[0][6] = lab06; labelArray[0][7] = lab07; labelArray[0][8] = lab08; labelArray[0][9] = lab09;
+         labelArray[1][0] = lab10; labelArray[1][1] = lab11; labelArray[1][2] = lab12; labelArray[1][3] = lab13; labelArray[1][4] = lab14; labelArray[1][5] = lab15; labelArray[1][6] = lab16; labelArray[1][7] = lab17; labelArray[1][8] = lab18; labelArray[1][9] = lab19;
+         labelArray[2][0] = lab20; labelArray[2][1] = lab21; labelArray[2][2] = lab22; labelArray[2][3] = lab23; labelArray[2][4] = lab24; labelArray[2][5] = lab25; labelArray[2][6] = lab26; labelArray[2][7] = lab27; labelArray[2][8] = lab28; labelArray[2][9] = lab29;
+         labelArray[3][0] = lab30; labelArray[3][1] = lab31; labelArray[3][2] = lab32; labelArray[3][3] = lab33; labelArray[3][4] = lab34; labelArray[3][5] = lab35; labelArray[3][6] = lab36; labelArray[3][7] = lab37; labelArray[3][8] = lab38; labelArray[3][9] = lab39;
+         labelArray[4][0] = lab40; labelArray[4][1] = lab41; labelArray[4][2] = lab42; labelArray[4][3] = lab43; labelArray[4][4] = lab44; labelArray[4][5] = lab45; labelArray[4][6] = lab46; labelArray[4][7] = lab47; labelArray[4][8] = lab48; labelArray[4][9] = lab49;
+         labelArray[5][0] = lab50; labelArray[5][1] = lab51; labelArray[5][2] = lab52; labelArray[5][3] = lab53; labelArray[5][4] = lab54; labelArray[5][5] = lab55; labelArray[5][6] = lab56; labelArray[5][7] = lab57; labelArray[5][8] = lab58; labelArray[5][9] = lab59;
+         labelArray[6][0] = lab60; labelArray[6][1] = lab61; labelArray[6][2] = lab62; labelArray[6][3] = lab63; labelArray[6][4] = lab64; labelArray[6][5] = lab65; labelArray[6][6] = lab66; labelArray[6][7] = lab67; labelArray[6][8] = lab68; labelArray[6][9] = lab69;
+         labelArray[7][0] = lab70; labelArray[7][1] = lab71; labelArray[7][2] = lab72; labelArray[7][3] = lab73; labelArray[7][4] = lab74; labelArray[7][5] = lab75; labelArray[7][6] = lab76; labelArray[7][7] = lab77; labelArray[7][8] = lab78; labelArray[7][9] = lab79;
+         labelArray[8][0] = lab80; labelArray[8][1] = lab81; labelArray[8][2] = lab82; labelArray[8][3] = lab83; labelArray[8][4] = lab84; labelArray[8][5] = lab85; labelArray[8][6] = lab86; labelArray[8][7] = lab87; labelArray[8][8] = lab88; labelArray[8][9] = lab89;
+         labelArray[9][0] = lab90; labelArray[9][1] = lab91; labelArray[9][2] = lab92; labelArray[9][3] = lab93; labelArray[9][4] = lab94; labelArray[9][5] = lab95; labelArray[9][6] = lab96; labelArray[9][7] = lab97; labelArray[9][8] = lab98; labelArray[9][9] = lab99;
 
 
 
